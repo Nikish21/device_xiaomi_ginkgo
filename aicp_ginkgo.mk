89a5bcd80fa2a06d9b16373b8825d07a2271119c
celtare21 <celtare21@gmail.com>
2020-04-29 11:04:55 +0000

    ginkgo: Aicp-fy
    Signed-off-by: celtare21 <celtare21@gmail.com>

--- aicp_ginkgo.mk
+++ aicp_ginkgo.mk
@@ -17,10 +17,10 @@ $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 $(call inherit-product, vendor/ANXCamera/config.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_ginkgo
+PRODUCT_NAME := aicp_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
